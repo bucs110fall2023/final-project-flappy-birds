19c5f665703fd1baf131ac69d63b1754b8e8bcde
Aug246 <agonzalez4@binghamton.edu>
2023-11-27 21:14:46 -0500

    movement class

--- src/movement.py
+++ src/movement.py
@@ -1,17 +1,26 @@
+import pygame
+
 class Movement:
     def __init__(self, x , y):
         self.x = x
         self.y = y
         
     def pipeMove(self):
-        self.x = 
-        pass
+        pipe_shift = (pygame.display.get_window_size()[0])/20
+        self.x = self.x - pipe_shift
+        return self.x
     
     def backgroundMove(self):
-        pass
+        back_shift = (pygame.display.get_window_size()[0])/20
+        self.x = self.x - back_shift
+        return self.x
     
     def birdMove(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.x = self.x
     
     def groundMove(self):
-        pass
+        ground_shift = (pygame.display.get_window_size()[0])/20
+        self.x = self.x - ground_shift
+        return self.x
